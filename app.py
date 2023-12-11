3c37bbf04194232d184c0d3c55c3c07631e962c5
asafasia <150935167+asafasia@users.noreply.github.com>
2023-12-11 22:17:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import streamlit as st
 from main_page import main_page
+from chip_parameters import chip_parameters
 
 
 page = st.sidebar.selectbox("Navigation", ["Main Page", "Chip Parameters", "Results Section"])
 
 if page == "Main Page":
     main_page()
+
+elif page == "Chip Parameters":
+    chip_parameters()
+
+
 
