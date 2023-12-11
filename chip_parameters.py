5c4d0562463b44ef11d83108bc68e72e2aedbbaa
asafasia <150935167+asafasia@users.noreply.github.com>
2023-12-11 22:22:11 +0200

    Update chip_parameters.py

--- chip_parameters.py
+++ chip_parameters.py
@@ -1,5 +1,5 @@
 import streamlit as st
-# import plotly.express as px
+import plotly.express as px
 import numpy as np
 
 def chip_parameters():
@@ -11,10 +11,10 @@ def chip_parameters():
     two_qubit_gate_fidelity = [0.99, 0.98, 0.97, 0.96]
 
     st.sidebar.title('Parameters')
-    #
-    # fig = px.bar_polar(T1, r="frequency", theta="direction", color="strength", template="plotly_dark",
-    #                    color_discrete_sequence=px.colors.sequential.Plasma_r)
+    
+    fig = px.bar_polar(T1, r="frequency", theta="direction", color="strength", template="plotly_dark",
+                       color_discrete_sequence=px.colors.sequential.Plasma_r)
 
-    # fig = px.bar_polar(r=T1, theta=np.array([1, 2, 3, 4, 5]) * 360, template="plotly_dark",
-    #                    color_discrete_sequence=px.colors.sequential.Plasma_r)
-    # st.plotly_chart(fig)
+    fig = px.bar_polar(r=T1, theta=np.array([1, 2, 3, 4, 5]) * 360, template="plotly_dark",
+                       color_discrete_sequence=px.colors.sequential.Plasma_r)
+    st.plotly_chart(fig)
