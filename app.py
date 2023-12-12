6ff694a15fd58605c8c1365f8477c0a38e6fa490
asafasia <150935167+asafasia@users.noreply.github.com>
2023-12-12 14:59:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,24 +2,5 @@ import streamlit as st
 from main_page import main_page
 import numpy as np
 
-# from chip_parameters import chip_parameters
 
 
-page = st.sidebar.selectbox("Navigation", ["Main Page", "Chip Parameters", "Results Section"])
-
-if page == "Main Page":
-    main_page()
-
-# elif page == "Chip Parameters":
-#     chip_parameters()
-
-
-
-x = np.linspace(0, 2 * np.pi, 100)
-y = np.sin(x)
-
-# Streamlit app
-st.title("Quantum Computing Plot")
-
-# Display the Matplotlib plot
-st.pyplot(plt.plot(x, y))
