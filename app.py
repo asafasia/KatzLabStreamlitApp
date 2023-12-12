4f3aa6f10b2ee7eb00457bfacdba171a0b242535
asafasia <150935167+asafasia@users.noreply.github.com>
2023-12-12 15:00:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,16 @@ import streamlit as st
 from main_page import main_page
 import numpy as np
 
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.title("Welcome to Nadav's Lab! 👋")
+st.write("We operate a state-of-the-art 5-qubit chip.")
+st.write("This app is designed to showcase the properties and results of experiments conducted on this chip")
+st.write("To access the results section, please use the sidebar.")
+
+
 
 
