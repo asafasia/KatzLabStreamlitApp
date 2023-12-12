7a10bac86addb63098d3c725543ae6a15dd3242c
asafasia <150935167+asafasia@users.noreply.github.com>
2023-12-12 16:01:38 +0200

    Update chip_parameters.py

--- pages/chip_parameters.py
+++ pages/chip_parameters.py
@@ -3,8 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from utils.a import b
 
-print(b)
-
 qubit_number = 5
 T1 = [50, 60, 70, 80, 90]
 T2 = [30, 40, 50, 60, 70]
@@ -14,14 +12,9 @@ two_qubit_gate_fidelity = [0.99, 0.98, 0.97, 0.96]
 
 st.sidebar.title('Parameters')
 
-# fig = px.bar_polar(T1, r="frequency", theta="direction", color="strength", template="plotly_dark",
-#                    color_discrete_sequence=px.colors.sequential.Plasma_r)
-#
-# fig = px.bar_polar(r=T1, theta=np.array([1, 2, 3, 4, 5]) * 360, template="plotly_dark",
-#                    color_discrete_sequence=px.colors.sequential.Plasma_r)
-# st.plotly_chart(fig)
-# x = np.linspace(0, 2 * np.pi, 100)
-# y = np.sin(x)
+
+x = np.linspace(0, 2 * np.pi, 100)
+y = np.sin(x)
 
 
 # fig = plt.figure()
